bfc27a9758ff3871fa1197766d2c084344cf438e
ClarissePllry <74117573+ClarissePllry@users.noreply.github.com>
2020-12-02 22:26:38 +0100

    Update port for clevercloud

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,6 +3,6 @@ import { LigneModule } from './ligne.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(LigneModule);
-  await app.listen(3000);
+  await app.listen(8080);
 }
 bootstrap();
